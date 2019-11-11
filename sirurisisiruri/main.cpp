49610637aee8187281c56dffe27524f155d7aa65
Nicolae <55551784+daimon18@users.noreply.github.com>
2019-11-11 19:56:04 +0200

    siruri si siruri 2.0

--- sirurisisiruri/main.cpp
+++ sirurisisiruri/main.cpp
@@ -7,8 +7,14 @@ int main()
     char sirStack[]="text la alegere";
     char* sirHeap= new char{sizeof(sirStack)};
         for(short i=0; i<sizeof(sirStack); i++)
-        sirHeap[i]= sirStack[i];
+       {
+           sirHeap[i]= sirStack[i];
             cout<<sirHeap<< endl;
             cout<<"numarul elementelor:"<<sizeof(sirStack)<<endl;
+       }
+        char varContor=1;
+            for(int j=0; j<=16; j++);
+             if (varContor=" ")
+             varContor++;
     return 0;
 }
